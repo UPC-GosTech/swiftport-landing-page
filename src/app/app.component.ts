5000433ab316dbc67c9a2c6eb184c9ddedb43e64
natalia <natalia.roman.cruz@gmail.com>
2025-04-19 03:02:41 -0500

    feat: Add why component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {
 } from '../components/funcionalidades-swiftport/funcionalidades-swiftport.component';
 import {BeneficiosComponent} from '../components/beneficios/beneficios.component';
 import {WhatComponent} from '../components/what/what.component';
+import {WhyComponent} from '../components/why/why.component';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ import {WhatComponent} from '../components/what/what.component';
     RouterOutlet,
     FuncionalidadesSwiftportComponent,
     BeneficiosComponent,
-    WhatComponent
+    WhatComponent,
+    WhyComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
