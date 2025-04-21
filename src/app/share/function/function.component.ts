745c20641f72d608f93bcf2bcf1133941a2e2db9
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-04-20 20:21:36 -0500

    chore: update styles of functionalidades section + Update of ngFor and ngIf

--- src/app/share/function/function.component.ts
+++ src/app/share/function/function.component.ts
@@ -1,12 +1,15 @@
 import {Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-function',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './function.component.html',
   styleUrl: './function.component.css'
 })
 export class FunctionComponent {
-  @Input() title: string = 'my title';
-  @Input() functionText: string = 'my text';
+  @Input() title: string = 'Característica';
+  @Input() functionText: string = 'Descripción de la funcionalidad ofrecida por SwiftPort.';
+  @Input() accentColor: string = '#EE812C';
 }
