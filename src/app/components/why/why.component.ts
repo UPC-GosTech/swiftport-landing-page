9d35c7b0ad003e3bbcaae95bff166cb09ac841d1
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-04-20 22:54:17 -0500

    chore: styles on how, what and why

--- src/app/components/why/why.component.ts
+++ src/app/components/why/why.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './why.component.css'
 })
 export class WhyComponent {
-
+  reasons = [
+    'Las industrias que operan en zonas remotas como la minera, petrolera o energética enfrentan diariamente una serie de desafíos logísticos complejos. Entornos hostiles e impredecibles, condiciones climáticas cambiantes, equipos técnicos expuestos a fallos, y la rotación constante del personal hacen que mantener la continuidad operativa sea una tarea difícil.',
+    'A esto se suma la falta de herramientas adecuadas para ofrecer trazabilidad, visibilidad en tiempo real y una planificación realmente adaptable.',
+    'En este contexto nace SwiftPort, como respuesta a la necesidad urgente de ordenar el caos operativo y ofrecer una solución capaz de centralizar la gestión logística, incluso en los escenarios más exigentes.'
+  ];
 }
