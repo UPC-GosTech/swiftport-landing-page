d3f9c9dc627348d7a784b7fd9f1feb45be380921
ghostrider101218 <142645808+ghostrider101218@users.noreply.github.com>
2025-04-20 20:06:08 -0500

    feat: header section implemented

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(event: MouseEvent): void {
+    if (this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
+  }
 }
