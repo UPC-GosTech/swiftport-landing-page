82e1490df29a9e2fcce184af8f74dfdab96e996e
Angelo <u20231B775@upc.edu.pe>
2025-04-22 22:26:07 -0500

    Feat:Add pricing-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import {FaqComponent} from './components/faq/faq.component';
 import {HeaderComponent} from './components/header/header.component';
 import { CommonModule } from '@angular/common';
 import {FooterComponent} from './components/footer/footer.component';
+import {PricingComponent} from './components/pricing/pricing.component';
 
 @Component({
   selector: 'app-root',
@@ -24,6 +25,7 @@ import {FooterComponent} from './components/footer/footer.component';
     FaqComponent,
     HeaderComponent,
     FooterComponent,
+    PricingComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
