94d31dc8ce52a996ca441fc98b6af7a939164a9f
natalia <natalia.roman.cruz@gmail.com>
2025-05-26 22:09:42 -0500

    feat: Fix functions i18n

--- src/app/share/function/function.component.ts
+++ src/app/share/function/function.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-function',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './function.component.html',
   styleUrl: './function.component.css'
 })
